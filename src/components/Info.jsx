dad1f4d2579a463410adf9aa3e35688a9439f54d
Poushik11 <115363626+Poushik11@users.noreply.github.com>
2023-09-02 12:16:52 +0530

    committed

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,7 +3,7 @@ function Info() {
   return (
     <div>
       <p className="info">
-        <h1 className="main-name">Hii,I am Poushik Yelne</h1>
+        <h1 className="main-name">Hi,I am Poushik Yelne</h1>
         <h2 className="sub-info">A Web Enthuist / A Frontend Developer</h2>
       </p>
     </div>
