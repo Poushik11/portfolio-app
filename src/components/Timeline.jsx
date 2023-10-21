2c3cd86cc4e98bb2fc714d0c9d16cc9e509267c4
Poushik11 <115363626+Poushik11@users.noreply.github.com>
2023-10-21 17:04:11 +0530

    "Background "

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,7 +1,63 @@
+import { useEffect, useRef } from "react";
 import "../styles/timeline.css";
+
 function Timeline() {
+  const timelineRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { root: null, rootMargin: "0px", threshold: 0.8 }
+    );
+
+    if (timelineRef.current) {
+      const timelineElements =
+        timelineRef.current.querySelectorAll(".timeline-content");
+      timelineElements.forEach((element) => {
+        observer.observe(element);
+      });
+    }
+  }, []);
+
   return (
-    <div className="timeline">
+    <div className="timeline" ref={timelineRef}>
+      <div className="timeline-block timeline-block-right">
+        <div className="marker"></div>
+        <div className="timeline-content">
+          <h3>First Year</h3>
+          <span>Some work experience</span>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate.
+          </p>
+        </div>
+      </div>
+
+      <div className="timeline-block timeline-block-left">
+        <div className="marker"></div>
+        <div className="timeline-content">
+          <h3>Second Year</h3>
+          <span>Some work experience</span>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate.
+          </p>
+        </div>
+      </div>
+
       <div className="timeline-block timeline-block-right">
         <div className="marker"></div>
         <div className="timeline-content">
@@ -19,52 +75,7 @@ function Timeline() {
       <div className="timeline-block timeline-block-left">
         <div className="marker"></div>
         <div className="timeline-content">
-          <h3>Seconed Year</h3>
-          <span>Some work experience</span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate.
-          </p>
-        </div>
-      </div>
-
-      <div className="timeline-block timeline-block-right">
-        <div className="marker"></div>
-        <div className="timeline-content">
-          <h3>Third Year</h3>
-          <span>Some work experience</span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate.
-          </p>
-        </div>
-      </div>
-
-      <div className="timeline-block timeline-block-left">
-        <div className="marker"></div>
-        <div className="timeline-content">
-          <h3>Fourth Year</h3>
-          <span>Some work experience</span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate.
-          </p>
-        </div>
-      </div>
-
-      <div className="timeline-block timeline-block-right">
-        <div className="marker"></div>
-        <div className="timeline-content">
-          <h3>Fifth Year</h3>
+          <h3>Second Year</h3>
           <span>Some work experience</span>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
